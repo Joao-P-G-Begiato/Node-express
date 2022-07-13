e3f862c7e901eafcd8d8428ece70a8a60d978db6
Joao P G Begiato <jpbegiato@hotmail.com>
2022-07-13 00:43:39 -0300

    adiciona a função pergunta e aplica os métodos da biblioteca

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
+const arr = []
+
+function pergunta(name) {
+    if(name !== "SAIR" && name !== "sair") {
+        arr.push(name)
+        readline.question("Digite uma propriedade de CSS: ", pergunta)
+    } else {
+        readline.close()
+        const result = arr.sort()
+        console.log( "\n Lista de propriedades CSS:")
+        for(let i = 0; i < result.length; i++) {
+            console.log(result[i])
+        }
+    }
+}
+
+readline.question("Digite uma propriedade de CSS:" , pergunta)
